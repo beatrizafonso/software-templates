2ed1affe21eeec38f2c0e83b196a7644cc246d4c
beatrizafonso <beatrizafonso@wine.com.br>
2021-12-28 17:57:25 -0300

    [java-template] add Java Application template

---
+++ my-templates/java/trigger/terraform/ci-gcp/triggers/winecombr/${{values.repo_name}}/main.tf
@@ -0,0 +1,34 @@
+locals {
+
+  # Common settings
+  account_name   = "winecombr"
+  git_folder     = "${{values.repo_name}}"
+  trigger_name   = "${{values.repo_name}}"
+
+  # Settings that deppend on the environment
+  environment = {
+    staging = {
+      branch_name = "^staging$"
+      env_name    = "staging"
+    }
+    production = {
+      branch_name = "^main$"
+      env_name    = "production"
+    }
+  }
+
+}
+
+module "${{values.repo_name}}" {
+  source          = "./../../../../../modules/gcp/cloud-build"
+  account_name    = local.account_name
+  for_each        = local.environment
+  git_folder      = local.git_folder
+  git_sub_folder  = local.git_sub_folder
+  trigger_name    = local.trigger_name
+  description     = "CI[${each.value["env_name"]}] ${local.account_name}/${local.git_folder} - ${local.git_sub_folder}"
+  branch_name     = each.value["branch_name"]
+  env_name        = each.value["env_name"]
+  cloudbuild_file = ".ci/cloudbuild.yaml"
+  watcher_name    = [{ branch = each.value["branch_name"] }]
+}
